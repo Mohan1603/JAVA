337a4c6a0b24b6ac15805f3035265d6ad58b8f38
Mohan1603 <mohan.16mar@gmail.com>
2021-07-26 16:06:55 +0530

    newq

---
+++ STS/myapp/myapp/src/test/java/com/sample/myapp/service/EmployeeServiceImpTest.java
@@ -0,0 +1,14 @@
+package com.sample.myapp.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeServiceImpTest {
+
+	@Test
+	void testSaveEmployee() {
+		fail("Not yet implemented");
+	}
+
+}
